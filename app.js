dec9d550b23bd64bd66b1228ff7a188f0c55343c
jiyeonjyn <ljy7183@naver.com>
2019-12-19 01:01:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ function onMouseMove(e) {
     }
 }
 
-function onMouseDown() {
+function onMouseDown(e) {
+    e.preventDefault();
     if (!filling) {
         painting = true;
     } else {
@@ -35,7 +36,8 @@ function onMouseDown() {
     }
 }
 
-function stopPainting() {
+function stopPainting(e) {
+    e.preventDefault();
     painting = false;
 }
 
